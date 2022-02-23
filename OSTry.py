c9b2465c3ab7129852c38fa31e9929e5a6127efa
ProfBrockway <84090839+ProfBrockway@users.noreply.github.com>
2022-02-23 07:55:27 -0500

    Create OSTry.py

---
+++ OSTry.py
@@ -0,0 +1,33 @@
+
+import os
+ 
+def ConsoleClear(): # Clear all output in console.
+    # This works regardless of Operating System prevailing.
+    try:
+        from IPython import get_ipython
+        get_ipython().magic("clear")
+        get_ipython().magic("reset -f")
+    except:
+        pass
+    return()  # End of function: Console_Clear 
+
+def Paths_Get():
+    # Get the current paths and file names of this app.
+    #   https://docs.python.org/3/library/os.path.html
+    FullPath = os.path.abspath(__file__)
+    print("\nThis app's full path and file name: \n","    ", FullPath )
+    
+    FileName = os.path.basename(FullPath)
+    print("\nThis app's file name: \n ","    ", FileName )
+    
+    ProjectPath = os.path.dirname(FullPath)
+    print("\nPath to this app without file name: \n","     ",ProjectPath )
+    
+    return()  # End of function: Paths_Get
+
+def Mainline():
+    ConsoleClear()  #Clear all output in console.
+    Paths_Get()
+    return()  # End of function: Mainline.
+        
+Mainline()   # Start run.
